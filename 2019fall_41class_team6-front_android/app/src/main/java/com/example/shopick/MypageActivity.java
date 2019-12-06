08504771598c75b8d137e7334f90cbc9ffc9b2fd
guswl98 <43779313+guswl98@users.noreply.github.com>
2019-12-07 05:22:41 +0900

    Update Mypage

--- 2019fall_41class_team6-front_android/app/src/main/java/com/example/shopick/MypageActivity.java
+++ 2019fall_41class_team6-front_android/app/src/main/java/com/example/shopick/MypageActivity.java
@@ -6,20 +6,29 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.facebook.login.LoginManager;
 
 public class MypageActivity extends AppCompatActivity {
     SharedPreferences loginPref;
+    TextView id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
 
-        Button fbLogoutButton = findViewById(R.id.logout);
-        fbLogoutButton.setOnClickListener(new View.OnClickListener() {
+        id=findViewById(R.id.id);
+        SharedPreferences prefs =getSharedPreferences("login", MODE_PRIVATE);
+        String result = prefs.getString("login", "0");
+        Log.d("ID:",result);
+        id.setText(result);
+
+        Button logout_btn = findViewById(R.id.logout);
+        logout_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 loginPref = getSharedPreferences("login", Activity.MODE_PRIVATE);
